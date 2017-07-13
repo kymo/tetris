3f3d8c2f9f4f9e3019edb913368f32d68ac57256
aron <412062385@qq.com>
2017-07-13 10:25:44 +0800

    delete file

--- team-framework/preview/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l),_(c,m,e,f,g,n),_(c,o,e,f,g,p),_(c,q,e,f,g,r),_(c,s,e,f,g,t)]);}; 
-var b="rootNodes",c="pageName",d="首页",e="type",f="Wireframe",g="url",h="首页.html",i="作品",j="作品.html",k="博客",l="博客.html",m="博文阅读",n="博文阅读.html",o="个人页面",p="个人页面.html",q="发表文章",r="发表文章.html",s="关于",t="关于.html";
-return _creator();
-})();
